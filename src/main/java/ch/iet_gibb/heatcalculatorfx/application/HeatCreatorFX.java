92393b4529d0d9af3e1db785ac81ce48ca5fb8e7
Noel Kohn1 <nko155535@stud.gibb.ch>
2025-10-23 11:06:40 +0200

    HeatCreatorFX erstellt

---
+++ src/main/java/ch/iet_gibb/heatcalculatorfx/application/HeatCreatorFX.java
@@ -0,0 +1,42 @@
+package ch.iet_gibb.heatcalculatorfx.application;
+
+
+import ch.iet_gibb.heatcalculatorfx.controller.HeatController;
+import ch.iet_gibb.heatcalculatorfx.model.HeatContainer;
+import ch.iet_gibb.heatcalculatorfx.model.RoofSolarSystemByArea;
+import ch.iet_gibb.heatcalculatorfx.model.RoofSolarSystemByCount;
+import ch.iet_gibb.heatcalculatorfx.model.SolarSystemOnSlope;
+import ch.iet_gibb.heatcalculatorfx.view.HeatView;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HeatCreatorFX extends Application{
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        List<HeatContainer> models = createModels();
+        HeatController controller = new HeatController(models);
+        HeatView view = new HeatView(stage, controller);
+        controller.setView(view);
+        controller.startView();
+    }
+
+    protected List<HeatContainer> createModels(){
+        ArrayList<HeatContainer> labels = new ArrayList<>();
+        HeatContainer roof = new RoofSolarSystemByArea(3000, 10, 5, 0.7, 4, 3);
+        labels.add(roof);
+        roof = new RoofSolarSystemByCount(6,3333, 1.4, 2);
+        labels.add(roof);
+        roof = new SolarSystemOnSlope(20, 2349, 4, 8, 2);
+        labels.add(roof);
+        return labels;
+    }
+}
